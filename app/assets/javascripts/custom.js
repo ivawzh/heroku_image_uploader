16a44dda7ba38b9002118816128c96be29ce45e2
ivawzh <raywzh@hotmail.com>
2014-07-05 16:21:09 +1000

    precompiled

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,21 +8,21 @@ $(function() {
                     alert('Maximum file size is 10 MB');
                     return false;
                 } else if(file.type.split('/')[0] !== "image" ){           //  file.type = "image/png", "video/avi", etc
-                    alert('Invalid file type. We only accept image formats JPG, GIF, BMP and PNG.')
+                    alert('Invalid file type. We only accept image formats JPG, GIF, BMP and PNG.');
                     return false;
                 } else {
                     return true;
                 }
             }
         }
-    );
+    )
 
     // error handling
-    $('#s3_uploader').bind('s3_upload_failed', function(e, content) {
+        .bind('s3_upload_failed', function(e, content) {
         return alert(content.filename + ' failed to upload:' + content.error_thrown);
-    });
-
-    $('#s3_uploader').bind("s3_upload_complete", function(e, content) {
+    })
+    // callback
+        .bind("s3_upload_complete", function(e, content) {
         var txt = "![Image of "+ content.filename + "](" + content.url + ")";
         var textarea = $("#ticket_description");
         textarea.val(textarea.val() + "\n" + txt);
